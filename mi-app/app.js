29eeee91468eed813b1f6f8ef426d144265f5d52
Pablo Garcia Alvarado <pablog.alvarado@proton.me>
2026-04-14 12:55:18 +0200

    workshop: completar workshop Docker basico

---
+++ mi-app/app.js
@@ -0,0 +1,6 @@
+const http = require('http');
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('<h1>Hola desde mi contenedor Docker!</h1>');
+});
+server.listen(3000, () => console.log('Servidor en puerto 3000'));
